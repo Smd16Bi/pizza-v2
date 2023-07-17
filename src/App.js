f0892938cb4a37a5feccc9113f67edd92b4d3522
Anton Vasylchenko <87824792+Smd16Bi@users.noreply.github.com>
2023-07-17 06:52:59 +0300

    install scss and create component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import "./scss/app.scss"
+import { Categories, Header, PizzaBlock, Sort } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">All pizza</h2>
+          <div className="content__items">
+            <PizzaBlock title="Mexico" price="500" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-
-
-
 export default App
